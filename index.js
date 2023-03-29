51f5800066f80ffa8cf61b2834b28d54dcc96c74
Vilmar Cazanatto <vilmarcazanatto@gmail.com>
2023-03-29 03:08:18 -0300

    changing method request

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log(req);
+app.post("/", (req, res) => {
+  console.log(req.body);
 
   res.sendStatus(200);
 });
